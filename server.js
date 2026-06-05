44c6365999bfea4e9471c917b39a615e2c1fc678
nicsipbd <nicsipbd@gmail.com>
2026-06-05 16:04:54 +0530

    Prepare backend for Render deployment

--- server.js
+++ server.js
@@ -344,6 +344,8 @@ io.on("connection", (socket) => {
 });
 
 //starts the server.
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
